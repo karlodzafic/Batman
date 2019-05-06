416def38743cd1dfabea17f95831a6e72d0b9a0d
MateIT92 <policmate@gmail.com>
2019-05-06 13:55:46 +0200

    Inicijalni commit frontend foldera izlet
    Upload datoteke frontend izleti (koje smo radili preko praznika)

---
+++ izlet frontend/javasaznaj.js
@@ -0,0 +1,7 @@
+document.getElementById('rega').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "flex";
+});
+
+document.querySelector('.zatvori').addEventListener("click", function() {
+	document.querySelector('.bg-modal').style.display = "none";
+});
