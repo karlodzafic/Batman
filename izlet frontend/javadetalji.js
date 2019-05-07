52bcaa7639057efcfbcb6f9cac9f0e6f18b73744
MateIT92 <policmate@gmail.com>
2019-05-07 20:07:08 +0200

    rad na stranici -detalji izleta
    dodana modalna forma za pisanje recenzije i ocjenjivanje izleta

--- izlet frontend/javadetalji.js
+++ izlet frontend/javadetalji.js
@@ -13,3 +13,20 @@ document.querySelector('.zatvori').addEventListener("click", function() {
 	document.querySelector('#CarouselTest').style.display = "flex";
 
 });
+
+
+document.getElementById('recenz').addEventListener("click", function() {
+
+	
+	document.querySelector('.bg-modal2').style.display = "flex";
+	document.querySelector('#CarouselTest').style.display = "none";
+
+
+});
+
+document.querySelector('.zatvori2').addEventListener("click", function() {
+	
+	document.querySelector('.bg-modal2').style.display = "none";
+	document.querySelector('#CarouselTest').style.display = "flex";
+
+});
