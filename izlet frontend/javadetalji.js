a9af91ae9f1c38f7e8991a8161d550543a689ef6
MateIT92 <policmate@gmail.com>
2019-05-08 13:47:54 +0200

    rad na kartici detalji izleta-dodana kao tab kartica, dodan templejt za popis korisnika ispravljanje modal recenzija .js
    probni komit ako zajebe ,zajebe

--- izlet frontend/javadetalji.js
+++ izlet frontend/javadetalji.js
@@ -18,6 +18,8 @@ document.querySelector('.zatvori').addEventListener("click", function() {
 document.getElementById('recenz').addEventListener("click", function() {
 
 	
+
+	document.querySelector('.overlay').style.display = "flex";
 	document.querySelector('.bg-modal2').style.display = "flex";
 	document.querySelector('#CarouselTest').style.display = "none";
 
@@ -26,6 +28,8 @@ document.getElementById('recenz').addEventListener("click", function() {
 
 document.querySelector('.zatvori2').addEventListener("click", function() {
 	
+	
+	document.querySelector('.overlay').style.display = "none";
 	document.querySelector('.bg-modal2').style.display = "none";
 	document.querySelector('#CarouselTest').style.display = "flex";
 
