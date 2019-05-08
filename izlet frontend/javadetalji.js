f7d1c0bf43e034e46415798d05816b87444548bf
MateIT92 <policmate@gmail.com>
2019-05-08 21:21:07 +0200

    u detalji izleta(javascript file) dodan js kod za ocjenjivanje
    dodan js kod za ocijenjivanje (Zvjezdice)
    koje se nalaze na modal formi kada korisnik stisne btn recenziraj

--- izlet frontend/javadetalji.js
+++ izlet frontend/javadetalji.js
@@ -38,3 +38,26 @@ document.querySelector('.zatvori2').addEventListener("click", function() {
  $(document).ready(function(){
       $("#datatables").DataTable();
     });
+
+
+    
+ function add(ths,sno){
+
+
+for (var i=1;i<=5;i++){
+var cur=document.getElementById("star"+i)
+cur.className="fa fa-star"
+}
+
+for (var i=1;i<=sno;i++){
+var cur=document.getElementById("star"+i)
+if(cur.className=="fa fa-star")
+{
+cur.className="fa fa-star checked"
+}
+}
+
+}
+
+
+    
